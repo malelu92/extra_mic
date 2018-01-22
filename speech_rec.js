c8e50092e6d1e1dad75f363ab04c284b304f1b54
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.fios-router.home>
2018-01-21 21:45:38 -0500

    trying to erase dialect

--- speech_rec.js
+++ speech_rec.js
@@ -73,25 +73,6 @@ var langs =
  ['日本語',           ['ja-JP']],
  ['Lingua latīna',   ['la']]];
 
-/*for (var i = 0; i < langs.length; i++) {
-  select_language.options[i] = new Option(langs[i][0], i);
-}
-select_language.selectedIndex = 6;
-updateCountry();
-select_dialect.selectedIndex = 6;
-showInfo('info_start');
-
-function updateCountry() {
-  for (var i = select_dialect.options.length - 1; i >= 0; i--) {
-    select_dialect.remove(i);
-  }
-  var list = langs[select_language.selectedIndex];
-  for (var i = 1; i < list.length; i++) {
-    select_dialect.options.add(new Option(list[i][1], list[i][0]));
-  }
-  select_dialect.style.visibility = list[1].length == 1 ? 'hidden' : 'visible';
-}*/
-
 var final_transcript = '';
 var recognizing = false;
 var ignore_onend;
