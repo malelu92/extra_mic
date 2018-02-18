48ca2b355c06d51abc95dfee7d1122f948ab64f1
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.fios-router.home>
2018-02-17 20:43:15 -0500

    changed error message color

--- speech_rec.js
+++ speech_rec.js
@@ -9,70 +9,6 @@ $(document).ready(function() {
   });
 });
 
-/*var langs =
-[['Afrikaans',       ['af-ZA']],
- ['Bahasa Indonesia',['id-ID']],
- ['Bahasa Melayu',   ['ms-MY']],
- ['Català',          ['ca-ES']],
- ['Čeština',         ['cs-CZ']],
- ['Deutsch',         ['de-DE']],
- ['English',         ['en-AU', 'Australia'],
-                     ['en-CA', 'Canada'],
-                     ['en-IN', 'India'],
-                     ['en-NZ', 'New Zealand'],
-                     ['en-ZA', 'South Africa'],
-                     ['en-GB', 'United Kingdom'],
-                     ['en-US', 'United States']],
- ['Español',         ['es-AR', 'Argentina'],
-                     ['es-BO', 'Bolivia'],
-                     ['es-CL', 'Chile'],
-                     ['es-CO', 'Colombia'],
-                     ['es-CR', 'Costa Rica'],
-                     ['es-EC', 'Ecuador'],
-                     ['es-SV', 'El Salvador'],
-                     ['es-ES', 'España'],
-                     ['es-US', 'Estados Unidos'],
-                     ['es-GT', 'Guatemala'],
-                     ['es-HN', 'Honduras'],
-                     ['es-MX', 'México'],
-                     ['es-NI', 'Nicaragua'],
-                     ['es-PA', 'Panamá'],
-                     ['es-PY', 'Paraguay'],
-                     ['es-PE', 'Perú'],
-                     ['es-PR', 'Puerto Rico'],
-                     ['es-DO', 'República Dominicana'],
-                     ['es-UY', 'Uruguay'],
-                     ['es-VE', 'Venezuela']],
- ['Euskara',         ['eu-ES']],
- ['Français',        ['fr-FR']],
- ['Galego',          ['gl-ES']],
- ['Hrvatski',        ['hr_HR']],
- ['IsiZulu',         ['zu-ZA']],
- ['Íslenska',        ['is-IS']],
- ['Italiano',        ['it-IT', 'Italia'],
-                     ['it-CH', 'Svizzera']],
- ['Magyar',          ['hu-HU']],
- ['Nederlands',      ['nl-NL']],
- ['Norsk bokmål',    ['nb-NO']],
- ['Polski',          ['pl-PL']],
- ['Português',       ['pt-BR', 'Brasil'],
-                     ['pt-PT', 'Portugal']],
- ['Română',          ['ro-RO']],
- ['Slovenčina',      ['sk-SK']],
- ['Suomi',           ['fi-FI']],
- ['Svenska',         ['sv-SE']],
- ['Türkçe',          ['tr-TR']],
- ['български',       ['bg-BG']],
- ['Pусский',         ['ru-RU']],
- ['Српски',          ['sr-RS']],
- ['한국어',            ['ko-KR']],
- ['中文',             ['cmn-Hans-CN', '普通话 (中国大陆)'],
-                     ['cmn-Hans-HK', '普通话 (香港)'],
-                     ['cmn-Hant-TW', '中文 (台灣)'],
-                     ['yue-Hant-HK', '粵語 (香港)']],
- ['日本語',           ['ja-JP']],
- ['Lingua latīna',   ['la']]];*/
-
 var final_transcript = '';
 var recognizing = false;
 var ignore_onend;
@@ -208,7 +144,7 @@ function getText(speech) {
     var arg = result[2]
 
     console.log("verb: " + verb + ", args: " + arg)
-    document.getElementById("demo").innerHTML = "";
+    document.getElementById("error").innerHTML = "";
 
     switch(verb) {
       case "click":
@@ -226,17 +162,17 @@ function getText(speech) {
         enterText(arg, clicked);
         if(clicked == undefined) {
           error_message = "please click on a text box";
-          document.getElementById("demo").innerHTML = error_message;
+          document.getElementById("error").innerHTML = error_message;
         }
         break;
       default:
         error_message = "sorry, that is not a recognized command";
-        document.getElementById("demo").innerHTML = error_message;
+        document.getElementById("error").innerHTML = error_message;
     }
   }
   else if(speech != ""){
     error_message = "sorry, that is not a recognized command";
-    document.getElementById("demo").innerHTML = error_message;
+    document.getElementById("error").innerHTML = error_message;
   }
   speech=null;
 }
@@ -278,7 +214,7 @@ function clickLink(text) {
   })
   if(valid_command == false) {
     error_message = "sorry, that is not a recognized command";
-    document.getElementById("demo").innerHTML = error_message;
+    document.getElementById("error").innerHTML = error_message;
   }
 }
 
